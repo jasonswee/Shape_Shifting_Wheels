d5e4032c88d2c6dc68ef54ab3849b2b102318cd1
Jason Swee <jason_swee@mymail.sutd.edu.sg>
2018-04-15 00:47:25 +0800

    RC car wheels and motor control simplify

--- RC_Shape_Shifting_Wheels/RC_Shape_Shifting_Wheels.ino
+++ RC_Shape_Shifting_Wheels/RC_Shape_Shifting_Wheels.ino
@@ -4,7 +4,7 @@
 */
 
 #include "Arduino.h"
-#define C1_PIN 1
+#define channel3_PIN 3
 #define MAX_POWER 100 
 #define E1 5 //M1 speed control     //PUMP
 #define M1 4 //M1 Directional control
@@ -37,26 +37,44 @@ int new_time_in_sec = 0;
 
 void state_excution()
 {
-  if(state == 0)//Normal
+  if(state == 0) //Lock valve nothing happen
   {
     analogWrite(E1,0); //Pump off
+    delay(50);//delay for valve
     analogWrite(E2,0); //Valve closed
   }
   else if(state == 1)//Inflating
   {
-    analogWrite(E1,power); //Pump off
-    analogWrite(E2,200); //Valve open
-  }
-  else if(state == 2)//Inflated
-  {
-    analogWrite(E1,0); //Pump closed
+    analogWrite(E1,power); //Pump on
+    delay(50);//delay for valve
     analogWrite(E2,0); //Valve closed
   }
   else if(state == 3)//Deflating
   {
-    analogWrite(E1,0); //Pump off
+    analogWrite(E1,0); //Pump closed
+    delay(50);//delay for valve
     analogWrite(E2,200); //Valve open
   }
+//  if(state == 0)//Normal
+//  {
+//    analogWrite(E1,0); //Pump off
+//    analogWrite(E2,0); //Valve closed
+//  }
+//  else if(state == 1)//Inflating
+//  {
+//    analogWrite(E1,power); //Pump off
+//    analogWrite(E2,200); //Valve open
+//  }
+//  else if(state == 2)//Inflated
+//  {
+//    analogWrite(E1,0); //Pump closed
+//    analogWrite(E2,0); //Valve closed
+//  }
+//  else if(state == 3)//Deflating
+//  {
+//    analogWrite(E1,0); //Pump off
+//    analogWrite(E2,200); //Valve open
+//  }
 }
 
 void remap()
@@ -67,8 +85,8 @@ void setup()
 {
   Serial.begin(9600);
   // when pin D2 goes high, call the rising function
-  pinMode(C1_PIN, INPUT);
-  attachInterrupt(digitalPinToInterrupt(C1_PIN), rising1, RISING);
+  pinMode(channel3_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(channel3_PIN), rising1, RISING);
 
   digitalWrite(M1,HIGH);
   digitalWrite(M2,HIGH);
@@ -79,6 +97,7 @@ void loop()
   new_time = millis();
   new_time_in_sec = new_time/1000;
   print_status(1000); 
+  
   remap();
   staging();
   state_excution();
@@ -87,57 +106,63 @@ void staging()
 {
   int starting_power_threshold = 70;
 
-  if(abs(c1)> starting_power_threshold)power = MAX_POWER * c1/100; //Power ranges -ve to +ve
-  else power = 0;
-  
-  if(state == 0) //Lock valve 
-  {
-    if(power > 0)
-    {
-      state = 3;
-      last_inflate_time = millis()/1000;
-    }   
-  }
-  else if(state == 1) //Inflating
-  {
-    lapsed_inflating_time = millis()/1000 - last_inflate_time;
-    if(lapsed_inflating_time >= MAX_INFLATE_TIME) 
-    {
-      last_deflate_time = millis()/1000;
-      state = 2;
-    }
-  }
-  else if(state == 2) //Inflated
-  {
-    if(power <0) 
-    {
-      state = 3;
-      last_deflate_time = millis()/1000;
-    }
-    
-  }
-  else if(state == 3) //deflating
-  {
-    lapsed_deflating_time = millis()/1000 - last_deflate_time;
-    if(lapsed_deflating_time >= DEFLATE_COOLDOWN) state =0;
-  }
+
+  //Check if c1 is above starting power threshold percentage
+  if(abs(c1)< starting_power_threshold)c1 = 0; 
+
+  if (c1 >0)state == 1; // inflate mode
+  else if (c1== 0) state == 0; //Lock mode
+  else if (c1<0) state == 2;//deflate mode
+
+  power = MAX_POWER*c1/100;
+//  if(state == 0) //Lock valve 
+//  {
+//    if(power > 0)
+//    {
+//      state = 3;
+//      last_inflate_time = millis()/1000;
+//    }   
+//  }
+//  else if(state == 1) //Inflating
+//  {
+//    lapsed_inflating_time = millis()/1000 - last_inflate_time;
+//    if(lapsed_inflating_time >= MAX_INFLATE_TIME) 
+//    {
+//      last_deflate_time = millis()/1000;
+//      state = 2;
+//    }
+//  }
+//  else if(state == 2) //Inflated
+//  {
+//    if(power <0) 
+//    {
+//      state = 3;
+//      last_deflate_time = millis()/1000;
+//    }
+//    
+//  }
+//  else if(state == 3) //deflating
+//  {
+//    lapsed_deflating_time = millis()/1000 - last_deflate_time;
+//    if(lapsed_deflating_time >= DEFLATE_COOLDOWN) state =0;
+//  }
   
 }
 void rising1() 
 {
-  attachInterrupt(digitalPinToInterrupt(C1_PIN), falling1, FALLING);
+  attachInterrupt(digitalPinToInterrupt(channel3_PIN), falling1, FALLING);
   prev_time1 = micros();
 }
 void falling1() 
 {
-  attachInterrupt(digitalPinToInterrupt(C1_PIN), rising1, RISING);
+  attachInterrupt(digitalPinToInterrupt(channel3_PIN), rising1, RISING);
   pwm_value1 = micros()-prev_time1;
 }
 void print_status(int print_every_x_millis)
 {
   if(millis()- last_print_time >= print_every_x_millis)
   {
-    Serial.print("Channel 1 PWM value(LR): ");
+    Serial.print("Channel 3 PWM value(LR): ");
     Serial.print(pwm_value1);
 
     Serial.print("  Calibrate 1: ");
@@ -149,6 +174,7 @@ void print_status(int print_every_x_millis)
     else if(state == 1) Serial.print("  Inflating  ");
     else if(state == 2) Serial.print("  Inflated  ");
     else if(state == 3) Serial.print("  deflating  ");
+    
     last_print_time = millis();
     Serial.print("      Time:");
     Serial.println(last_print_time/1000);
